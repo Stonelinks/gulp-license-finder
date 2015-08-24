92a1c8671764de6804ec9f0b35977e47ffc5caf2
Lucas Doyle <ldoyle@airware.com>
2015-08-24 14:54:09 -0700

    custom renderer

--- index.js
+++ index.js
@@ -16,50 +16,97 @@ var Stream = require('stream');
 // Consts
 var PLUGIN_NAME = 'gulp-license-finder';
 
+var delim = '\t';
+var heading = ['serviceName', 'name', 'version', 'license', 'directory'].join(delim);
+
+/**
+ * Returns a single csv line for a module record
+ *
+ * @param  {Object} moduleRecord The module's license record
+ * @return {String}              The record turned into a TSV record
+ */
+function recordToTsv(moduleRecord) {
+
+    // @todo - what do do if any of the data contains a comma?
+    // put double quotes around the value?
+    return moduleRecord.name + delim +
+        moduleRecord.version + delim +
+        moduleRecord.summary().join(';') + delim +
+        moduleRecord.directory;
+}
+
+/**
+ * Render the license data
+ *
+ * @param  {Array}    licenseData An array of module licence data
+ * @param  {String}   serviceName name of the service to be prepended to the row
+ * @param  {Function} callback    The callback (err, output string)
+ */
+function render(licenseData, serviceName, callback) {
+
+    if (typeof callback !== 'function') {
+        throw new Error('must have a callback');
+    }
+
+    if (!Array.isArray(licenseData)) {
+        return callback(new Error('licenseData must be an array'));
+    }
+
+    if (licenseData.length < 1) {
+        return callback(new Error('must have at least one module in data'));
+    }
+
+    var output = [heading];
+
+    licenseData.forEach(function (module) {
+        output.push(serviceName + delim + recordToTsv(module));
+    });
+
+    callback(null, output.join('\n'));
+}
+
 function licenseFinder(filename, options) {
 
-	// if the filename is an object, then
-	// we will assume this is just an options
-	// object
-	if (typeof filename === 'object') {
-		options = filename;
-		filename = options.filename;
-	}
+    // if the filename is an object, then
+    // we will assume this is just an options
+    // object
+    if (typeof filename === 'object') {
+        options = filename;
+        filename = options.filename;
+    }
 
-	// set default options
-	filename = filename || 'licenses.txt';
-	options = options || {};
-	options.directory = options.directory || process.cwd();
-	options.production = options.production || false;
-	options.csv = options.csv || false;
-	options.depth = options.depth; // undefined is the default
+    // set default options
+    filename = filename || 'licenses.tsv';
+    options = options || {};
+    options.directory = options.directory || process.cwd();
+    options.production = options.production || false;
+    options.serviceName = options.serviceName || ''; // used to namespace to a service
+    options.depth = options.depth; // undefined is the default
 
-	var stream = new Stream();
+    var stream = new Stream();
 
-	nlf.find(options, function (err, data) {
-		if (err) {
-			throw new gutil.PluginError(PLUGIN_NAME, err, {showStack: true});
-		}
+    nlf.find(options, function (err, data) {
+        if (err) {
+            throw new gutil.PluginError(PLUGIN_NAME, err, {showStack: true});
+        }
 
-		var formatter = options.csv ? nlf.csvFormatter : nlf.standardFormatter;
+        render(data, options.serviceName, function (err, output) {
+            if (err) {
+                throw new gutil.PluginError(PLUGIN_NAME, err, {showStack: true});
+            }
 
-		formatter.render(data, function (err, output) {
-			if (err) {
-				throw new gutil.PluginError(PLUGIN_NAME, err, {showStack: true});
-			}
+            var file = new File({
+                path: path.join(process.cwd(), filename),
+                contents: new Buffer(output)
+            });
 
-			var file = new File({
-				path: path.join(process.cwd(), filename),
-				contents: new Buffer(output)
-			});
+            stream.emit('data', file);
+            stream.emit('finish');
+        });
 
-			stream.emit('data', file);
-			stream.emit('finish');
-		});
+    });
 
-	});
-
-	return stream;
+    return stream;
 }
 
 module.exports = licenseFinder;
